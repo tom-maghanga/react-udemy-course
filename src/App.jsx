8064c1b640b246f9f9b42b8b8678a4dc3f126730
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2023-11-26 10:51:54 +0300

    setState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () =>{
-    let count = 0;
+   const [tasks, setTasks] = useState([{id:5011 , name:"Learn Reactjs", completed : false},
+   {id:5012 , name:" Florida Banner", completed : false},
+   {id:5012 , name:"RBK webiste project timeline", completed : false}]);
+    
     return(
         <div className="App">
-            <div className="box">
-                <p>{count}</p>
-                <button className='add'>Add</button>
-                <button className='subtract'>Subtract</button>
-            </div>
-        </div>
+  <h1>Task List</h1>
+  <ul className='tasks'>
+    {tasks.map((task) => (
+      <li key={task.id} className='task'>
+        {task.name}
+      </li>
+    ))}
+  </ul>
+</div>
     )
 }
 
