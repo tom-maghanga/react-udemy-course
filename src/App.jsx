f44be78f15a9a1962af41d93f7fc4f226bdca0fb
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2023-11-26 17:01:18 +0300

    Conditional templates

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,28 @@ const App = () =>{
    const [tasks, setTasks] = useState([{id:5011 , name:"Learn Reactjs", completed : false},
    {id:5012 , name:" Florida Banner", completed : false},
    {id:5012 , name:"RBK webiste project timeline", completed : false}]);
+
+   const[show, setShow] = useState(true);
+
+   function handleDetele(id){
+    setTasks(tasks.filter(task => task.id !== id));
+   }
     
     return(
         <div className="App">
   <h1>Task List</h1>
   <ul className='tasks'>
-    {tasks.map((task) => (
+    <button onClick={()=> setShow(true)}>Show</button>
+    <button onClick={() =>setShow(false)}>Hide</button>
+
+     {show && tasks.map((task) => (
       <li key={task.id} className='task'>
         {task.name}
+
+        <button onClick={()=>(handleDetele(task.id))}>Delete</button>
       </li>
-    ))}
+    ))    
+   }
   </ul>
 </div>
     )
